1e47875bd9e12b463d5212e1f90442eeac376e41
test <test@qq.com>
2024-07-28 14:53:05 +0800

    增加说明

--- 04_icmp/icmp.c
+++ 04_icmp/icmp.c
@@ -31,33 +31,33 @@ static uint16_t gSrcPort;
 static uint16_t gDstPort;
 
 #endif
-
+// 定义一个端口的id表示的是 绑定的网卡id
 int gDpdkPortId = 0;
-
+// 设置端口配置信息
 static const struct rte_eth_conf port_conf_default = {
         .rxmode = {.max_rx_pkt_len = RTE_ETHER_MAX_LEN}
 };
 
 static void ng_init_port(struct rte_mempool *mbuf_pool) {
-
-    uint16_t nb_sys_ports = rte_eth_dev_count_avail(); //
+    // 检测端口是否可用 dpdk 绑定了多少个网卡 这里应该是多少个
+    uint16_t nb_sys_ports = rte_eth_dev_count_avail();
     if (nb_sys_ports == 0) {
         rte_exit(EXIT_FAILURE, "No Supported eth found\n");
     }
-
+    // 获取默认的第一个网口
     struct rte_eth_dev_info dev_info;
-    rte_eth_dev_info_get(gDpdkPortId, &dev_info); //
+    rte_eth_dev_info_get(gDpdkPortId, &dev_info); // 第一个网口信息
 
-    const int num_rx_queues = 1;
-    const int num_tx_queues = 1;
+    const int num_rx_queues = 1;// 接受 最多可以设置八个 因为有八个CPU当前机器
+    const int num_tx_queues = 1;// 发送 暂时这个版本 不发送数据 先设置0
     struct rte_eth_conf port_conf = port_conf_default;
+    // 多队列网卡的意思是 设置这个端口的配置信息
+    // 用于配置以太网设备的全局参数。它通常在设置特定队列之前调用，用于初始化和配置设备的总体属性和行为
     rte_eth_dev_configure(gDpdkPortId, num_rx_queues, num_tx_queues, &port_conf);
-
-
-    // Invalid value for nb_tx_desc(=128), should be: <= 4096, >= 512, and a product of 1 调整为1024
+    // 用于配置和初始化特定 RX 队列的参数，包括 RX 描述符的数量、NUMA 节点和内存池。
+    // 127 指的是可以堆积这么多 队列里面的数量是128
     if (rte_eth_rx_queue_setup(gDpdkPortId, 0, 1024,
                                rte_eth_dev_socket_id(gDpdkPortId), NULL, mbuf_pool) < 0) {
-
         rte_exit(EXIT_FAILURE, "Could not setup RX queue\n");
 
     }
@@ -66,13 +66,14 @@ static void ng_init_port(struct rte_mempool *mbuf_pool) {
     struct rte_eth_txconf txq_conf = dev_info.default_txconf;
     txq_conf.offloads = port_conf.rxmode.offloads;
     // 第二个参数 对应的是那个队列 第三个参数对应的队列最大承载多少
+    // Invalid value for nb_tx_desc(=128), should be: <= 4096, >= 512, and a product of 1 调整为1024
     if (rte_eth_tx_queue_setup(
             gDpdkPortId, 0, 1024,
             rte_eth_dev_socket_id(gDpdkPortId), &txq_conf) < 0) {
         rte_exit(EXIT_FAILURE, "发送队列设置失败\n");
     }
 #endif
-
+    // 设置完之后 启动接收队列
     if (rte_eth_dev_start(gDpdkPortId) < 0) {
         rte_exit(EXIT_FAILURE, "Could not start\n");
     }
@@ -233,9 +234,10 @@ int main(int argc, char *argv[]) {
     if (mbuf_pool == NULL) {
         rte_exit(EXIT_FAILURE, "Could not create mbuf pool\n");
     }
-
+    // 初始化网口的信息
     ng_init_port(mbuf_pool);
-    rte_eth_macaddr_get(gDpdkPortId, (struct rte_ether_addr *) gSrcMac); // 原mac地址获取
+    // 获取绑定的网口mac地址
+    rte_eth_macaddr_get(gDpdkPortId, (struct rte_ether_addr *) gSrcMac);
     while (1) {
 
         struct rte_mbuf *mbufs[BURST_SIZE]; // 也可以设置大一点128个
@@ -256,6 +258,9 @@ int main(int argc, char *argv[]) {
                         rte_pktmbuf_mtod_offset(mbufs[i], struct rte_arp_hdr*, sizeof(struct rte_ether_hdr));
                 struct in_addr addr;
                 // 调试代码
+                addr.s_addr = arp_hdr->arp_data.arp_sip; // 发送方的IP
+                printf("arp ---> 发送方IP: %s \n", inet_ntoa(addr));
+                // 发送的目标IP
                 addr.s_addr = arp_hdr->arp_data.arp_tip; // 某个机器发送的目标IP
                 printf("arp ---> src: %s ", inet_ntoa(addr));
                 addr.s_addr = gLocalIp;
@@ -264,7 +269,7 @@ int main(int argc, char *argv[]) {
                 // 目标IP发送本机的时候才处理 如果没有if的判断就是一个arp攻击
                 if (arp_hdr->arp_data.arp_tip == gLocalIp) {
                     struct rte_mbuf *arpmbuf = ng_send_arp(mbuf_pool, arp_hdr->arp_data.arp_sha.addr_bytes,
-                                                          arp_hdr->arp_data.arp_tip, arp_hdr->arp_data.arp_sip);
+                                                           arp_hdr->arp_data.arp_tip, arp_hdr->arp_data.arp_sip);
                     rte_eth_tx_burst(gDpdkPortId, 0, &arpmbuf, 1);
                     rte_pktmbuf_free(arpmbuf);
                     rte_pktmbuf_free(mbufs[i]);
@@ -329,73 +334,6 @@ int main(int argc, char *argv[]) {
 
     }
 
-}
-
-// 下面是测试的代码
-// 定义一个端口的id表示的是 绑定的网卡id
-int gDpdkPortIdT = 0;
-// 设置端口配置信息
-static const struct rte_eth_conf port_conf_defaultT = {
-        .rxmode = {.max_rx_pkt_len = RTE_ETHER_MAX_LEN} //
-};
-
-static void ng_init_portT(struct rte_mempool *mbuf_pool) {
-    // 检测端口是否可用 dpdk 绑定了多少个网卡 这里应该是多少个
-    uint16_t nb_sys_ports = rte_eth_dev_count_avail(); //
-    if (nb_sys_ports == 0) {
-        rte_exit(EXIT_FAILURE, "没有可用的网卡\n");
-    }
-    // 获取默认的第一个网口
-    struct rte_eth_dev_info dev_info;
-    rte_eth_dev_info_get(gDpdkPortIdT, &dev_info); // 第一个网口信息
-
-    const int num_rx_queues = 1; // 接受 最多可以设置八个 因为有八个CPU当前机器
-    const int num_tx_queues = 0; // 发送 暂时这个版本 不发送数据 先设置0
-    // 多队列网卡的意思是 设置这个端口的配置信息
-    // 用于配置以太网设备的全局参数。它通常在设置特定队列之前调用，用于初始化和配置设备的总体属性和行为
-    rte_eth_dev_configure(gDpdkPortIdT, num_rx_queues, num_tx_queues, &port_conf_defaultT);
-    // 用于配置和初始化特定 RX 队列的参数，包括 RX 描述符的数量、NUMA 节点和内存池。
-    // 127 指的是可以堆积这么多 队列里面的数量是128
-    int rte_eth_set_res = rte_eth_rx_queue_setup(gDpdkPortIdT, 0, 128,
-                                                 rte_eth_dev_socket_id(gDpdkPortId), NULL, mbuf_pool);
-    // 如果设置失败不能启动
-    if (rte_eth_set_res < 0) {
-        rte_exit(EXIT_FAILURE, "设置接收队列失败\n");
-    }
-    // 设置完之后 启动接收队列
-    if (rte_eth_dev_start(gDpdkPortIdT) < 0) {
-        rte_exit(EXIT_FAILURE, "启动失败\n");
-    }
-}
-
-// 增加原型不会有警告：‘test_main’先前没有原型 [-Wmissing-prototypes]
-//  int test_main(int argc, char *argv[]) {
-//      ^
-// cc1: 警告：无法识别的命令行选项“-Wno-address-of-packed-member” [默认启用]
-int test_main(int argc, char *argv[]);
-
-int test_main(int argc, char *argv[]) {
-
-    if (rte_eal_init(argc, argv) < 0) {
-        rte_exit(EXIT_FAILURE, "大页初始化失败");
-    }
-    // 初始化内存池 DPDK 一个进程确定一个内存池 内存会放在这个变量中
-    // 设置4K 8K都是可以的 这里我们设置一个特殊的值 不去满足2的N次方 比如设置4096-1的好处  小于4k的放在4K里面 大于4K的 放在另外大于4K的地方
-
-    // 初始化内存池
-    struct rte_mempool *mbuf_pool = rte_pktmbuf_pool_create("mbuf pool", NUM_MBUFS,
-                                                            0, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-    if (mbuf_pool == NULL) {
-        rte_exit(EXIT_FAILURE, "内存池初始化失败");
-    }
-    // 初始化获取网卡驱动
-    ng_init_portT(mbuf_pool);
-
-    while (1) {
-        // 接受数据的时候 包的数据量最大可以写入128个
-        // 如果超过128 可能会出错 机器网卡可能会重启 、丢弃还是重启 还不太确定 ，超出了机器可能会重启 或者宕机 具体要看什么情况
-        // rte_eth_rx_burst();
-    }
 }
 
 
